79ad63855105caab732250fa3d93c17d6edfab1e
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-05-10 03:18:57 -0300

    feat: início da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, NavLink } from 'react-router-dom';
+import './App.css';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
 import Home from './pages/Home/Home';
@@ -10,7 +11,7 @@ function App() {
     <div className="main-container">
       <nav className="navbar">
         <NavLink to="/" end className="nav-link">Home</NavLink>
-        <NavLink to="/sobre-mim" className="nav-link">Sobre Mim</NavLink>
+        <NavLink to="/sobre-mim" className="nav-link">Sobre mim</NavLink>
         <NavLink to="/contato" className="nav-link">Contato</NavLink>
         <NavLink to="/projetos" className="nav-link">Projetos</NavLink>
         <NavLink to="/skills" className="nav-link">Skills</NavLink>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,11 @@
+import profilePicture from '../../assets/Raisa - cabelo verde.jpg';
+import './Home.module.css';
+
 function Home() {
   return (
     <div>
       <h4>Home</h4>
+      <img src={ profilePicture } alt="foto de perfil" />
     </div>
   );
 }
