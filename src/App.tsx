b22c3bce3595f3850cab8331e1c8c1602feb511f
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-05-10 02:23:54 -0300

    chore: criação do diretório pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
+import Skills from './pages/Skills/Skills';
 
 function App() {
   return (
-    <div />
+    <div className="main-container">
+      <nav className="navbar">
+        <NavLink to="/" end className="nav-link">Home</NavLink>
+        <NavLink to="/sobre-mim" className="nav-link">Sobre Mim</NavLink>
+        <NavLink to="/contato" className="nav-link">Contato</NavLink>
+        <NavLink to="/projetos" className="nav-link">Projetos</NavLink>
+        <NavLink to="/skills" className="nav-link">Skills</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/sobre-mim" element={ <About /> } />
+        <Route path="/contato" element={ <Contact /> } />
+        <Route path="/projetos" element={ <Projects /> } />
+        <Route path="skills" element={ <Skills /> } />
+      </Routes>
+    </div>
   );
 }
 
