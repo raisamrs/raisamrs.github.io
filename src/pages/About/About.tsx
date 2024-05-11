423874016fe385c1449f07959900fb30f461eaf9
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-05-11 06:34:50 -0300

    feat: inserção de texto na home

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,6 +2,23 @@ function About() {
   return (
     <div>
       <h4>Sobre mim</h4>
+      <p> Oiê! Seja bem-vinde! 😊 </p>
+      {/* eslint-disable-next-line max-len */}
+      <p>Tenho 32 anos, sou de Salvador - BA, tenho 32 anos e sou mãe de pet! 🐕</p>
+      <p>
+        {/* eslint-disable-next-line max-len */}
+        Com background na área de Qualidade de Software, adquiri habilidades essenciais trabalhando como testadora para algumas consultorias e tive o privilégio de contribuir em projetos desafiadores para clientes renomados como B3 e Itaú. Recentemente, decidi realizar uma transição de subárea, percebendo que o desenvolvimento de software me oferecerá maior realização profissional.
+      </p>
+      <p>
+        {/* eslint-disable-next-line max-len */}
+        Eu estou empenhada em aprimorar minhas habilidades como desenvolvedora web, e para isso estou cursando a
+        <a href="https://www.betrybe.com/formacao-desenvolvimento-web"> formação em Desenvolvimento Full Stack na Trybe</a>
+        .
+      </p>
+      <p>
+        {/* eslint-disable-next-line max-len */}
+        Minha paixão pelas cores e minha experiência no mundo da arte (literalmente, pinto e bordo! Hahaha!) me impulsionam especialmente em direção ao front-end, onde  é possível combinar minha criatividade com habilidades técnicas para criar boas experiências visuais impactante para os usuários.
+      </p>
     </div>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
-import profilePicture from '../../assets/Raisa - cabelo verde.jpg';
-import './Home.module.css';
+import profilePicture from '../../assets/Raisa - cabelo lilás.png';
+import styles from './Home.module.css';
 
 function Home() {
   return (
-    <div>
-      <h4>Home</h4>
+    <div className={ styles.homeContainer }>
       <img src={ profilePicture } alt="foto de perfil" />
+      <h3>Olá, Mundo! Eu sou</h3>
+      <h1>Raisa Moreno</h1>
+      <h3> Desenvolvedora Web Front-End</h3>
     </div>
   );
 }
