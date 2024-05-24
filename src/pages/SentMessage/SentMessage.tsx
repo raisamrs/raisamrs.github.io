4f11a285ed2f81aa25e71e6cfe23d8f0ce2576c0
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-05-23 23:54:51 -0300

    feat: criação do componente para confirmação de mensagem enviada

---
+++ src/pages/SentMessage/SentMessage.tsx
@@ -0,0 +1,11 @@
+function SentMessage() {
+  return (
+    <div>
+      <h1> Mensagem enviada com sucesso!</h1>
+      <h2>Obrigada pelo seu contato! </h2>
+      <h2>Te responderei em até 3 dias úteis!😉</h2>
+    </div>
+  );
+}
+
+export default SentMessage;
