7071baa14d8e8307b7a9385d183c2dc8bf9ff8f6
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-05-24 20:25:16 -0300

    styles: manipulação das classes por meio da biblioteca classname

---
+++ src/helpers/classesNames.ts
@@ -0,0 +1,33 @@
+import classNames from 'classnames';
+import styles from '../pages/Home/Home.module.css';
+
+export const myNameClasses = classNames(
+  styles.myName,
+  'display-3',
+  'text-uppercase',
+  'mb-2',
+);
+export const professionClasses = classNames(
+  styles.typedText,
+);
+export const helloWorldClasses = classNames(
+  styles.helloWorld,
+  'font-weight-normal',
+  'mb-3',
+);
+export const textContentClasses = classNames(
+  styles.textContent,
+  'mt-5',
+  'pt-5',
+  'mb-3',
+  'mx-5',
+  'px-4',
+);
+export const iAmClasses = classNames(
+  styles.iAm,
+  'mb-3',
+);
+
+export const imgClasses = classNames(
+  styles.imgContainer,
+);
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,44 +1,57 @@
 import { useEffect, useState, useRef } from 'react';
 import classNames from 'classnames';
+import {
+  myNameClasses, professionClasses, helloWorldClasses, iAmClasses, imgClasses,
+  textContentClasses,
+} from '../../helpers/classesNames';
 import profilePicture from '../../assets/Raisa - cabelo lilás.png';
 import styles from './Home.module.css';
 
 function Home() {
   const [text, setText] = useState('');
-  const fullText = 'Desenvolvedora Web Front-End';
+  const [showCursor, setShowCursor] = useState(true);
+  const fullText = '< Desenvolvedora Web Front-End />';
   const speed = 100;
-  const indexRef = useRef(0); // Usar useRef para manter o índice
+  const indexRef = useRef(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       const i = indexRef.current;
       if (i < fullText.length) {
         setText((prev) => prev + fullText.charAt(i));
+        setShowCursor((prev) => !prev); // Alterna a visibilidade do cursor
         indexRef.current += 1; // Incrementar o índice
       } else {
         clearInterval(intervalId);
+        setTimeout(() => {
+          setText('');
+          indexRef.current = 0;
+        }, speed);
       }
     }, speed);
 
     return () => clearInterval(intervalId); // Limpar intervalo quando o componente desmonta
-  }, [fullText, speed]);
-
-  const myNameClasses = classNames(styles.myName, 'display-3', 'text-uppercase', 'mb-2');
-  const professionClasses = classNames(
-    styles.typedText,
-    'font-weight-lighter',
-    'text-white',
-  );
+  }, [text, fullText, speed]);
 
   return (
     <div className={ styles.homeContainer }>
-      <img src={ profilePicture } alt="foto de perfil" />
-      <h3 className="text-white font-weight-normal mb-3">Olá, Mundo! Eu sou</h3>
-      <h1 className={ myNameClasses }>Raisa Moreno</h1>
-      <h3 className={ professionClasses }>
-        {text}
-        <span className={ styles.cursor } />
-      </h3>
+      <div className={ imgClasses }>
+        <img
+          src={ profilePicture }
+          alt="foto de perfil"
+        />
+      </div>
+      <div className={ textContentClasses }>
+        <h2 className={ helloWorldClasses }>&lt; Olá, Mundo! /&gt;</h2>
+        <h3 className={ iAmClasses }>Eu sou</h3>
+        <h1 className={ myNameClasses }>Raisa Moreno</h1>
+        <h3 className={ professionClasses }>
+          {text}
+          <span
+            className={ classNames(styles.cursor, { [styles.hidden]: !showCursor }) }
+          />
+        </h3>
+      </div>
     </div>
   );
 }
