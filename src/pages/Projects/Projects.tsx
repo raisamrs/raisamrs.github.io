f725af49a2cb682ab9ecceb735caa0f04e4223ad
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-05-23 23:54:15 -0300

    feat: renderização dos projetos

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,8 +1,37 @@
+import projectsData from '../../data/projects.json';
+import styles from './Projects.module.css';
+import pixelsArt from '../../assets/pixelsArt.png';
+import { ProjectImageType } from '../../types';
+
 function Projects() {
+  const projectImages: ProjectImageType = {
+    pixelsArt,
+  };
+
   return (
-    <div>
+    <>
       <h4>Projetos</h4>
-    </div>
+      <div className="cards-container">
+        {projectsData.map((project, i) => (
+          <div key={ i } className={ styles.projectCard }>
+            <h5 className="projectTitle">{ project.title }</h5>
+            <p className="projectDescription">{project.description}</p>
+            <img src={ projectImages[project.id] } alt={ project.title } />
+            <a href={ project.github } target="_blank" rel="noreferrer">
+              {' '}
+              <p>GitHub</p>
+            </a>
+            <a href={ project.deploy } target="_blank" rel="noreferrer">
+              {' '}
+              <p>Deploy</p>
+              {project.technologies.map((tech, j) => (
+                <p className="technologies" key={ j }>{tech}</p>
+              ))}
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 export default Projects;
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type ProjectImageType = {
+  [key: string]: string;
+};
