1c19e15deca56aaaa902c532253a381150454491
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-05-25 01:35:13 -0300

    feat: mudança do arquivo classesNames da Home para a pasta home

---
+++ src/pages/Home/classesNames.ts
@@ -0,0 +1,46 @@
+import classNames from 'classnames';
+import styles from './Home.module.css';
+
+export const helloWorldClasses = classNames(
+  styles.helloWorld,
+  'font-weight-normal',
+  'mb-3',
+);
+export const homeContainerClasses = classNames(
+  styles.homeContainer,
+);
+export const iAmClasses = classNames(
+  styles.iAm,
+  'mb-3',
+);
+export const imgProfileClasses = classNames(
+  styles.imgProfileContainer,
+);
+export const myNameClasses = classNames(
+  styles.myName,
+  'display-3',
+  'text-uppercase',
+  'mb-2',
+);
+export const professionClasses = classNames(
+  styles.typedText,
+);
+
+export const socialNetWorkClasses = classNames(
+  styles.socialNetWork,
+  'mx-3',
+);
+export const socialNetWorksContainerClasses = classNames(
+  styles.socialNetWorksContainer,
+  'text-center',
+  'mt-3',
+);
+export const textContainerClasses = classNames(
+  styles.textContainer,
+);
+
+export const textImageContainerClasses = classNames(
+  styles.textImageContainer,
+  'mt-5',
+  'pt-5',
+);
