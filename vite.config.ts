a1453c23e76d1b6539e2fe857521b6262bbc9dfb
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-06-05 23:17:59 -0300

    chore: configurações de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
-  }
-})
+  base: "/",
+});
